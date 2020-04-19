171a102e2e2134644602d91cbe3da81088194f73
Redrix <44963837+Redrix@users.noreply.github.com>
2020-04-20 01:11:38 +0200

    Add files via upload

--- monScript.js
+++ monScript.js
@@ -1,9 +1,30 @@
+//Déifinition des liens et titres par défaut
+if(localStorage.getItem("nomBoites") === null || localStorage.getItem("sections") === null || localStorage.getItem("liens") === null){
+    localStorage.setItem("nomBoites", '["Social","Divertissement","Prog","Utile"]');
+    localStorage.setItem("sections", '[["Twitter", "Facebook", "Reddit", "Discord", ""], ' +
+    '["Furaffinity", "Youtube", "Spotify", ""], ' +
+    '["Bootstrap", "StackOverflow", "GitHub", ""], ' +
+    '["PayPal", "OutlookOffice", "Portail Mediaschool", ""]]');
+    localStorage.setItem("liens", '[["https://twitter.com/", "https://facebook.com/", "https://www.reddit.com/", "https://discordapp.com/", ""],' +
+        '["https://furaffinity.net/", "https://www.youtube.com/", "https://www.spotify.com/", "", ""],' +
+        '["https://getbootstrap.com/", "https://stackoverflow.com/", "https://github.com/", "", ""],' +
+        '["https://www.paypal.com/" ,"https://login.microsoftonline.com/", "http://portail.mediaschool.eu/", "", ""]]');
+}else{
+    console.log("Tu as déjà les noms <:3");
+}
+
+let titreBoite = document.getElementsByClassName("titreBoite");
+let sectionsBoites = document.getElementsByClassName("section");
+let titreEnregistres = JSON.parse(localStorage.getItem("nomBoites"));
+let sectionEnregistrees = JSON.parse(localStorage.getItem("sections"));
+let liensEnregistrees = JSON.parse(localStorage.getItem("liens"));
 let nombreDeClick = 0;
 
 afficherHeure(); // Afficher l'heure sur la page
 afficherDate(); // Afficher la date sur la page
 getQuote(); // Afficher une phrase aléatoire
 addEvent(); // Ajouter un event listener a chaque moteur de recherche
+initialisation(); //Permet de mettre les titres et liens perso
 
 document.getElementById("API").addEventListener("click", easterEgg);
 
@@ -200,5 +221,70 @@ function changeActive(){
         case "https://www.graphiline.com/src/applications/news/imaloader/images/graphiline/logo-icone-youtube-2017.jpg":
             document.getElementById("recherche").setAttribute("action", "https://www.youtube.com/results");
         break;
+    }
+}
+
+function initialisation(){ //Afficher les liens et titres perso
+    let compteurDiv = 0;
+    for(let i = 0; i < 4; i++){
+        titreBoite[i].innerHTML = titreEnregistres[i] + ' <i class="far fa-edit modificateur" onclick="modifier(' + i + ')"></i>';
+        for(let y = 0; y < 5; y++){
+            if(liensEnregistrees[i][y] === "" || sectionEnregistrees[i][y] === ""){
+                sectionsBoites[compteurDiv].innerHTML = "";
+                console.log("Vide");
+            }else{
+                sectionsBoites[compteurDiv].innerHTML = '<a href="' + liensEnregistrees[i][y] + '">' + sectionEnregistrees[i][y] + '</a>';
+                console.log(sectionEnregistrees[i][y]);
+            }
+            compteurDiv += 1;
+        }
+    }
+}
+
+function modifier(boiteAModifier){ //Choisir quoi modifier dans une boite
+    let aModifier = prompt('Saisir "1" pour modifier le titre\nSaisir "2" pour modifier les liens', "");
+    if (aModifier === "1"){
+        modifierTitreBoite(boiteAModifier);
+    }
+    if(aModifier === "2"){
+        modifierSectionBoite(boiteAModifier);
+    }
+}
+
+function modifierTitreBoite(numeroBoite){ //Modifier le titre d'une boite
+    let choixUtilisateur = prompt('Donnez le nouveau titre de "' + titreEnregistres[numeroBoite] + '"', "");
+    if(choixUtilisateur !== null){
+        titreEnregistres[numeroBoite] = choixUtilisateur;
+        localStorage.setItem("nomBoites", JSON.stringify(titreEnregistres));
+    }
+}
+
+function modifierSectionBoite(numeroBoite){ //Modifier un lien d'une boite
+    let affichage = "";
+    for(let i = 0; i < 5; i++){
+        if(sectionEnregistrees[numeroBoite][i] !== ""){
+            affichage += 1 + i + ". " + sectionEnregistrees[numeroBoite][i] + "\n";
+        }else{
+            affichage += 1 + i + ". Vide \n";
+        }
+        console.log(i);
+    }
+    let numeroSection = prompt("Donnez le numéro du lien a changer \n" + affichage, "") - 1;
+    if (numeroSection !== null && numeroSection < 5 && numeroSection >= 0){
+        let nouveauNom = prompt('Donnez le nouveau nom de cette section\nSaisir "-" pour la laisser vide', "");
+        if (nouveauNom === "-"){
+            sectionEnregistrees[numeroBoite][numeroSection] = "";
+            liensEnregistrees[numeroBoite][numeroSection] = "";
+            localStorage.setItem("sections", JSON.stringify(sectionEnregistrees));
+            localStorage.setItem("liens", JSON.stringify(liensEnregistrees));
+        }else{
+            let nouveauLien = prompt("Collez le lien vers lequel cette section doit vous mener", "");
+            if(nouveauLien !== null && nouveauNom !== null){
+                sectionEnregistrees[numeroBoite][numeroSection] = nouveauNom;
+                liensEnregistrees[numeroBoite][numeroSection] = nouveauLien;
+                localStorage.setItem("sections", JSON.stringify(sectionEnregistrees));
+                localStorage.setItem("liens", JSON.stringify(liensEnregistrees));
+            }
+        }
     }
 }
